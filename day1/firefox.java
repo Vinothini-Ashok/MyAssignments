d61b2e0ad3352eb35cee01ba479e59b6ad3d6fc7
Vinothini-Ashok <vinothini.othisamy@gmail.com>
2026-01-03 21:02:49 +0530

    Add files via upload

---
+++ day1/firefox.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class firefox {
+
+	public static void main(String[] args) {
+		float browserVersion=100.2f;
+		String browserName="firefox";
+		boolean isvisible=true;
+		int releaseYear=1998;
+		char browserLogo='F';
+		System.out.println(browserVersion+"  "+browserName+"  "+isvisible+" "+releaseYear+" "+browserLogo);
+		
+
+	}
+
+}
